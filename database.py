b338705ef4dd98c6fe402972b4beda5ba2ddebd0
teod0036 <73177141+teod0036@users.noreply.github.com>
2025-05-22 22:22:41 +0200

    fixed a bug that caused the database initialization script to add the same data multiple times

--- database.py
+++ database.py
@@ -31,20 +31,22 @@ def init_db():
 
     for value in player_data['Team'].unique():  
         team_name_query = """
-        INSERT INTO teams(team_name, points, wins, losses, draws) VALUES('%s', NULL, NULL, NULL, NULL);
+        INSERT INTO teams(team_name, points, wins, losses, draws) VALUES('%s', NULL, NULL, NULL, NULL) ON CONFLICT DO NOTHING;
         """ % (value)
         cur.execute(team_name_query)
 
-    for row in player_data.to_dict('records'):
-        player_data_query = """
-        INSERT INTO players(pid, player_name, nation, age, team_name) VALUES(DEFAULT, '%s', '%s', %i, '%s');
-        """ % (row['Player'], row['Nation'], row['Age'], row['Team'])
-        cur.execute(player_data_query)
+    for record in player_data.to_dict('records'):
+        cur.execute("""SELECT 1 FROM players WHERE player_name = '%s' AND nation = '%s' AND age = %i AND team_name = '%s' """ % (record['Player'], record['Nation'], record['Age'], record['Team']))
+        if cur.fetchone is None:
+            player_data_query = """
+            INSERT INTO players(pid, player_name, nation, age, team_name) VALUES(DEFAULT, '%s', '%s', %i, '%s') ON CONFLICT DO NOTHING;
+            """ % (record['Player'], record['Nation'], record['Age'], record['Team'])
+            cur.execute(player_data_query)
 
-    for row in match_data.to_dict('records'):
+    for record in match_data.to_dict('records'):
         match_data_query = """
-        INSERT INTO matches_played(round, result, opponent, team_name) VALUES('%i', '%s', '%s', '%s');
-        """ % (row['Round'], row['Result'], row['Opponent'], row['Team'])
+        INSERT INTO matches_played(round, result, opponent, team_name) VALUES('%i', '%s', '%s', '%s') ON CONFLICT DO NOTHING;
+        """ % (record['Round'], record['Result'], record['Opponent'], record['Team'])
         cur.execute(match_data_query)
     
 
